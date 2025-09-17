cede31d8950b3af76e08d8fdd6304310081c9b51
DWWM <thomas.coment@gmail.com>
2025-09-17 16:06:01 +0200

    feat:ajout dossier petsitting

---
+++ petsitting/model/species/species_one.php
@@ -0,0 +1,13 @@
+<?php
+
+$id = 2;
+
+require_once '../pdo.php';
+require_once '../../utils.php';
+
+$sql = 'SELECT * FROM species WHERE id=:id';
+$q = $pdo->prepare($sql);
+$q->bindValue(':id', $id, PDO::PARAM_INT);
+$q->execute();
+
+$result = $q->fetch();
